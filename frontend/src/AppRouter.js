dc282687d39a10903654811a5bc0c9934c586988
George Batsis <georgebatsis95@gmail.com>
2024-07-11 21:09:54 +0300

    Description of changes for issue #7

---
+++ frontend/src/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+function AppRouter() {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+}
+
+export default AppRouter;
